21d5dc1b9763a17798ca7607c5cf93e76abe0396
ThomasJd <thomasdinh25@gmail.com>
2018-07-22 23:50:10 -0500

    Redux

---
+++ src/components/Postform.js
@@ -0,0 +1,24 @@
+//ES7 React/Redux/Graphql
+
+import React, { Component } from 'react'
+
+class Postform extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+         <h1> Add Post</h1>
+
+       </div>
+    )
+  }
+}
+
+export default Postform
